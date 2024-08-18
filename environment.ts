a433d5e2cfd5339553fe82febb39e4fc762e980d
Alucia <analuciacarhuaz@gmail.com>
2024-08-17 21:45:40 -0500

    update url api

--- environment.ts
+++ environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   backend: {
-    url: 'http://localhost:3000/api'
+    url: 'https://reto-tecnico-backend-production.up.railway.app/api'
+    // url: 'http://localhost:3000/api'
   }
 };
